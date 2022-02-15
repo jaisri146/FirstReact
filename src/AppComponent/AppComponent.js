ae4584bf7c995f239499d253b17d7778263aacc9
unknown <you@example.com>
2022-02-15 16:07:43 +0530

    react project

--- src/AppComponent/AppComponent.js
+++ src/AppComponent/AppComponent.js
@@ -1,9 +1,18 @@
 import React from "react";
+import NewComponent from '../NewComponent/NewComponent';
+import ParaComponent from '../ParaComponent/ParaComponent';
+import HeadComponent from '../HeadComponent/HeadComponent';
+
+
 
 const AppComponent = () =>{
     return(
-       <>
-       </>
+       <div>
+        <NewComponent/>
+        <ParaComponent/>
+        <HeadComponent/>
+
+       </div>
     )
 }
 export default AppComponent;
---
+++ src/NewComponent/NewComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../NewComponent/NewComponent.css';
+
+
+const NewComponent=()=>
+{
+    return(
+    <div className="new">
+        <h1 >Beautiful & consistant UI <br></br>powered with<span className="niv">React.</span> </h1>
+        
+    </div>
+    )
+}
+export default NewComponent;
+
